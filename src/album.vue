6a42c0421cf33d846569661ba3fe062954c028ee
张会亮 <crazyhuiliang@163.com>
2018-08-05 15:56:57 +0800

    通讯录

---
+++ src/album.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+
+  </div>
+</template>
+
+<script>
+import {
+  WxcButton
+} from 'weex-ui'
+export default {
+  name: '',
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  },
+  components: {
+    WxcButton
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+  }
+</style>
--- src/friendList.vue
+++ src/friendList.vue
@@ -10,8 +10,9 @@
       <text class="new-frined-text">新的朋友</text>
     </div>
     <div class="friend-list">
-      <div class="friend" v-for="(item, i) in friendsList" :key="i">
-        <text>{{item}}</text>
+      <div class="friend" v-for="(user) in friendsList" :key="user.id" @click="userClicked(user)">
+        <image style="width:50px;height:50px" :src="user.avatar"></image>
+        <text style="flex: 1; margin-left: 20px;">{{user.nickname}}</text>
       </div>
     </div>
     <tab class="tab" :activeIndex="0" @tabClicked="tabClicked"></tab>
@@ -23,6 +24,7 @@ import {
   WxcButton,
   WxcMinibar
 } from 'weex-ui'
+import store from './store/index'
 import Tab from './components/Tab'
 import {getEntryUrl} from './util/util'
 import {getFriendsList} from './api/index'
@@ -66,13 +68,23 @@ export default {
     },
 
     loadFiendsList () {
-      getFriendsList(1).then(({data}) => {
+      store.dispatch('getUserInfo').then(userInfo => {
+        return getFriendsList(userInfo.id)
+      }).then(({data}) => {
         if (data.state) {
           this.friendsList = data.info
         }
       }, (error) => {
         console.log(error)
       })
+    },
+    userClicked (user) {
+      // navigator.push({
+      //   url: getEntryUrl('userCenter'),
+      //   animated: 'true'
+      // }, event => {
+      //   // modal.toast({ message: 'callback: ' + event })
+      // })
     }
   },
   components: {
@@ -102,5 +114,17 @@ export default {
   .new-frined-text {
     line-height: 80px;
     text-align: center;
+  }
+
+  .friend {
+    padding-top: 30px;
+    padding-right: 30px;
+    padding-bottom: 30px;
+    padding-left: 30px;
+    flex-direction: row;
+    justify-content: space-between;
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    border-bottom-color: #dddddd;
   }
 </style>
